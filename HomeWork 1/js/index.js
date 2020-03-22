8160ae176bee5184f02db12afa996e1c7e5a52f6
wfds <61466199+wfds@users.noreply.github.com>
2020-03-22 22:40:33 +0200

    HW1

---
+++ HomeWork 1/js/index.js
@@ -0,0 +1,53 @@
+console.log("My first debug")
+
+
+let age = 0
+const petName = "Lisa"
+console.log(age,petName)
+
+
+age = 12
+console.log(age,petName)
+
+
+const numberOfPlayers = 12
+console.log(typeof numberOfPlayers)
+const isPro = true
+console.log(typeof isPro)
+const team = "LA1994"
+console.log(typeof team)
+const userSecretQuestion = null
+console.log(typeof userSecretQuestion)
+const teamName = {
+    playersCount: 12,
+    playersName: "Wesg",
+    isFree: false
+}
+console.log(typeof teamName)
+const numbersOfSchool = [1,2,3,4,5]
+console.log(typeof numbersOfSchool)
+
+
+
+const student ={
+    name: "Den",
+    surname: "Medvedskyi",
+    isActive: true
+}
+console.log(student["isActive"])
+
+
+
+const fruits = [1,2,3,4,5]
+const numbers = [1,2,3,4,5]
+const students = [1,2,3,4,5]
+const objectMassive = [1,2,3,4,5]
+
+
+
+const numbers10 = [1,2,3,4,5,6,7,8,9,10]
+console.log(numbers10["0"])
+console.log(numbers10["9"])
+
+
+console.log(student)
