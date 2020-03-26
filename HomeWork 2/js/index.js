4adf6afeb7aa3ad4337b0adc06fab326dc2c276c
wfds <61466199+wfds@users.noreply.github.com>
2020-03-26 15:21:40 +0200

    HW2

---
+++ HomeWork 2/js/index.js
@@ -0,0 +1,129 @@
+console.log(1 == 2)
+console.log(1 != 2)
+console.log(1 == 1)
+console.log(1 !=1)
+console.log("text" == 5)
+console.log("text" != 5)
+console.log("text" == "Text")
+console.log("text" == "text")
+console.log(true == "true")
+console.log(true == false)
+console.log(false == 0)
+console.log(false === 0)
+console.log("" == false)
+console.log("" !== false)
+console.log(undefined === null)
+console.log(undefined == null)
+console.log(17 === true)
+console.log("17" === true)
+console.log({} === {})
+
+const evenNumber = Number(window.prompt("Type a number", ""));
+window.alert(evenNumber % 2 === 0)
+
+const oddNumber = Number(window.prompt("Type a number", ""));
+window.alert(oddNumber % 2 !== 0)
+
+let cd = 12
+cd -= 5
+console.log(cd)
+let vd = 12
+vd /= 4
+console.log(vd)
+let bd = 12
+bd **= 3
+console.log(bd)
+
+
+let a = "100"
+console.log(-a)
+console.log(Number(a))
+console.log("100" / "1")
+
+let q = 3
+let w = 4
+console.log((q **= 2) - (w **= 3))
+
+
+
+let nn = Number(window.prompt("Type a number", ""));
+let hh = Number(window.prompt("Type a number", ""));
+let gg = Number(window.prompt("Type a number", ""));
+nn += 10
+hh += 10
+gg += 10
+window.alert(nn + hh + gg)
+
+
+const n = Number(window.prompt("Type a number", ""));
+const h = Number(window.prompt("Type a number", ""));
+const g = Number(window.prompt("Type a number", ""));
+window.alert(n + h > g, h + g > n, n + g > h)
+
+let l = Number(window.prompt("Type a first number", ""));
+let k = Number(window.prompt("Type a second number", ""));
+console.log((l + k)** 3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
