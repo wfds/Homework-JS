3acaeebca7d2842313e7db1429dafa91aa7fcd94
wfds <61466199+wfds@users.noreply.github.com>
2020-06-09 16:25:45 +0300

    Update index.js

--- HomeWork 6/js/index.js
+++ HomeWork 6/js/index.js
@@ -1,3 +1,9 @@
+
+function a (b)
+{
+    alert(b);
+}
+
 function a (){
     let name = prompt('Enter your name')
     alert('Hello,' + name)
@@ -17,36 +23,32 @@ cube()
 
 
 
-function avg2() {
-    let c = +prompt('введите первое число')
-    let d = +prompt('введите второе число')
+function avg2(c,d) {
     let result = ((c+d)/2)
     
     return result;
 }
+avg2(1,2)
+avg2(10,5)
 
 
 
 
-
-function sum3() {
-    var a = parseInt(prompt('Первое число')) || 0;
-    var b = parseInt(prompt('Второе число')) || 0;
-    var c = parseInt(prompt('Третье число')) || 0;
+function sum3(a=0,b=0,c=0) {
     return a+b+c;
   }
-  console.log(sum3())
+  sum3(1,2,3)
+  sum3(5,10,100500)
+  sum3(5,10)
 
 
-//Здесь есть вопрос
-//Функция работает как нужно но только через промпт, не могу понять как без промпта присвоить параметрам функции число 0 в случае если не указан диапазон
-function intRandom(min,max) {
-    if(min || max === undefined){
-        min = prompt('min') || 0;
-        max = prompt('min') || 0;}
-        let rand = min - 0.5 + Math.random() * (max - min + 1)
-        return Math.round(rand)
-    }
+
+
+
+function intRandom(min=0,max=0) {
+    let rand = min - 0.5 + Math.random() * (max - min + 1)
+    return Math.round(rand)
+}
 
 intRandom(2,15) // возвращает целое случайное число от 2 до 15 (включительно)
 intRandom(-1,-1) // вернет -1
@@ -55,12 +57,12 @@ intRandom(10) //вернет 0 до 10 включительно
 
 
 
-//Функция работает,только не совсем понятно с запятой в алерте,без нее все пишется слитно в строке,а если добавляю ее как строчку в цикле в конце строки то она получается завершает приветствие и вылазит даже если параметр всего один,для нее ведь явно не нужно делать отдельный if,как это лучше воплотить?
+
 function greetAll(name) {
     var result = '';
     var i;
  for (i = 0; i < arguments.length; i++) {
-       result += arguments[i] + ',';
+       result += ',' +  arguments[i];
     }
     return alert(`Hello ${result}`);
  }
